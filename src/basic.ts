ffb07bb74e8eeb770cfebf0c1012aeb6ac1c3cd4
Hideki Koike <hdkkik@gmail.com>
2025-04-23 23:12:47 +0900

    fix typo

--- src/basic.ts
+++ src/basic.ts
@@ -8,7 +8,7 @@ let isDone = false;
 // Explicit type valiables
 let explicitMessage: string = "this is explicit";
 let explicitCount: number = 20;
-let expliitIsDone: boolean = true;
+let explicitIsDone: boolean = true;
 
 let numbers: number[] = [1, 2, 3, 4, 5];
 let strings: string[] = ["a", "b", "c"];
@@ -16,7 +16,7 @@ let inferredNumbers = [4,5,6];
 let inferredStrings = ["x", "y", "z"];
 
 function add(n1: number, n2: number): number {
-    let result = n1 - n2;
+    let result = n1 + n2;
     console.log(result);
     return result;
 }
@@ -24,5 +24,5 @@ function add(n1: number, n2: number): number {
 add(5,9);
 
 console.log(message, count, isDone);
-console.log(explicitMessage, explicitCount, expliitIsDone);
+console.log(explicitMessage, explicitCount, explicitIsDone);
 console.log(numbers, strings, inferredNumbers, inferredStrings);
